e148630c3c24f53135383b7e014f44f36d820edb
Jordan Marinov <jaisor@gmail.com>
2024-10-27 22:06:49 -0700

    reverting away from react

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React, { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
+import express, { json } from 'express';
 import "./styles/main.scss";
-import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <StrictMode>
-   <App />
-  </StrictMode>
-);
+const app = express();
+
+app.use(json())
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', async (req, res) => {
+    res.json({ status: true, message: "Our node.js app works" })
+});
+
+app.listen(PORT, () => console.log(`App listening at port ${PORT}`));
