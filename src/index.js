dee6763fb6e81c8df6daf33baee86fe6a82ad504
Jordan Marinov <jaisor@gmail.com>
2024-10-30 22:50:42 -0700

    working pico css with ejs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv'
 import logger from 'winston'
 import express, { json } from 'express'
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 logger.configure({
   level: process.env.LOG_LEVEL || 'info',
@@ -11,19 +13,21 @@ logger.configure({
 // Config
 dotenv.config()
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const server = express()
 
 //server.use(json())
 
-server.set('view engine', 'ejs');
-//server.use(express.static(__dirname + '/public'));
+server.set('view engine', 'ejs')
+server.use(express.static('public'))
 server.get('/', function(req, res) {
-  res.render('pages/index');
-});
+  res.render('pages/index')
+})
 
 server.get('/', async (req, res) => {
     res.json({ status: true, message: "Our node.js app works" })
-});
+})
 
 const port = process.env.PORT || 8080
 server.listen(port, () => logger.info(`Server listening to ${port}`))
